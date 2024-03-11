5e65fdcb4e80006dddd416a63632b414aa17acf9
vtouzard <vtouzard244@gmail.com>
2024-03-11 12:22:03 +0100

    feat: Ajout de la liste de recette reçus depuis l'API

--- src/context/index.tsx
+++ src/context/index.tsx
@@ -5,39 +5,61 @@ import React, {
   FormEventHandler,
 } from "react";
 
+type RecipeType = {
+  id: string;
+  image_url: string;
+  publisher: string;
+  title: string;
+};
+
 ///-----LE CONTEXT -----////
 // Typage pour le context
 export type ContextType = {
   searchParam: string;
   setSearchParam: (param: string) => void;
-  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+  handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
+  loading: boolean;
+  recipeList: RecipeType[];
 };
 
 // On exporte le context pour y accèder dans l'application
 export const GlobalContext = createContext<ContextType | undefined>(undefined);
 ///-----LE CONTEXT -----////
 
-
 // Ceci est le fournisseur de context
 // il permet de passer des informations sans avoir besoins de passer par les props
 // dans notre application, il est utilisé au niveaux du fichier "index.tsx"
 // Afin de l'utiliser sur l'entièreté de l'application
 export const GlobalState = ({ children }: React.PropsWithChildren) => {
-  const [searchParam, setSearchParam] = useState('');
+  const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
 
-  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) =>{
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-        const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`)
-        const data = await res.json();
-        console.log(data);
+      const res = await fetch(
+        `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      );
+      const data = await res.json();
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+      }
     } catch (error) {
-        console.error(error)
+      console.error(error);
+      setLoading(false);
+      setSearchParam("");
     }
-  }
+  };
+
+  console.log(loading, recipeList);
 
   return (
-    <GlobalContext.Provider value={{ searchParam, setSearchParam, handleSubmit }}>
+    <GlobalContext.Provider
+      value={{ searchParam, setSearchParam, handleSubmit, loading, recipeList }}
+    >
       {children}
     </GlobalContext.Provider>
   );
@@ -45,9 +67,9 @@ export const GlobalState = ({ children }: React.PropsWithChildren) => {
 
 // Hook personnalisé pour utiliser le contexte dans les composants enfants
 export const useGlobalState = () => {
-    const context = useContext(GlobalContext);
-    if (!context) {
-        throw new Error('useGlobalState must be used within a GlobalStateProvider');
-    }
-    return context;
+  const context = useContext(GlobalContext);
+  if (!context) {
+    throw new Error("useGlobalState must be used within a GlobalStateProvider");
+  }
+  return context;
 };
