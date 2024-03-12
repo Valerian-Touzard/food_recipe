e4dbfdf247969119c64d9a6c934db976b25d2e95
vtouzard <vtouzard244@gmail.com>
2024-03-12 14:12:27 +0100

    feat: Ajout de l'affichage des recettes mises en favoris

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { useGlobalState } from "../../context";
+import RecipeItem from "../../components/recipe-item";
 
 const Favorites = () => {
-  return (
-    <div>Favorites</div>
-  )
-}
+  const { favoritesList } = useGlobalState();
 
-export default Favorites
+  return (
+    <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+      {favoritesList && favoritesList.length > 0 ? (
+        favoritesList.map((favorite) => (
+          <RecipeItem key={favorite.id} recipe={favorite} />
+        ))
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+            Nothing is added in favorites.
+          </p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Favorites;
